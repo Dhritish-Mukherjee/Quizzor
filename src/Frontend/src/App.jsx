322a3ce3ebb69355525a2ea2002673fd73210a38
Codewithpabitra <pabitra05.maity@gmail.com>
2025-12-01 11:53:34 +0530

    Frontend : project setup done

---
+++ src/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Quizzor_logo from './assets/Quizzor_logo.png'
+
+
+const App = () => {
+  return (
+    <div className='w-full h-screen justify-center items-center bg-zinc-900 mx-auto text-white'>
+     welcome to Quizzor.AI
+    </div>
+  )
+}
+
+export default App
