89f7a0551c4054ca63eb1017b9005ca946f7b913
Codewithpabitra <pabitra05.maity@gmail.com>
2025-12-01 12:29:36 +0530

    git issue solved

--- src/Frontend/src/App.jsx
+++ src/Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Quizzor_logo from './assets/Quizzor_logo.png'
 
 
+
+
 const App = () => {
   return (
     <div className='w-full h-screen justify-center items-center bg-zinc-900 mx-auto text-white'>
