50c1ba450695c746b18891c2de98e20c3810d1e3
Codewithpabitra <pabitra05.maity@gmail.com>
2025-12-02 13:39:44 +0530

    navbar improved

--- src/Frontend/src/components/Navbar.jsx
+++ src/Frontend/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 const Navbar = () => {
   return (
     <div>
-      <nav className="flex justify-between py-4 ">
-        <Link to='/' ><div className="logo border border-gray-600 px-5 py-1.5  rounded-full">
-            <h1 className="text-white text-xl">Quizzor.<span className="text-blue-500 text-lg ">ai</span></h1>
+      <nav className="flex justify-between py-4 px-4">
+        <Link to='/' ><div className="logo border border-gray-600 px-5 py-1 rounded-full flex justify-center items-center">
+            <h1 className="text-white text-xl">Quizzor<span className="text-blue-500 text-2xl ">.</span>ai</h1>
         </div>
         </Link>
 
         <div className="right-menu flex items-center justify-center gap-3 ">
-            <button className="px-5 py-1.5 bg-blue-500 rounded-full text-neutral-100 hover:scale-105 transition-all duration-300 cursor-pointer">Login</button>
+            <button className="px-5 py-1 bg-blue-600 rounded-full text-neutral-100 hover:scale-105 hover:bg-blue-500 transition-all duration-300 cursor-pointer">Sign in</button>
 
-            <button className="px-4 py-1.5 bg-transparent rounded-full border border-gray-600 hover:bg-blue-500 hover:border-blue-500 hover:scale-105 transition-all duration-300 text-neutral-100 cursor-pointer">Go to Dashboard</button>
+            <button className="px-5 py-1 bg-transparent rounded-full border border-neutral-100 hover:text-neutral-300  transition-all duration-300 text-neutral-100 cursor-pointer flex justify-center items-center gap-1">Go to Dashboard <IoIosArrowRoundForward/></button>
         </div>
       </nav>
     </div>
