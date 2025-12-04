c08d13f419de925df0a784a942f68b0eaf89de0b
Codewithpabitra <pabitra05.maity@gmail.com>
2025-12-04 21:23:06 +0530

    auth and logout functionality added

--- src/Frontend/src/components/Navbar.jsx
+++ src/Frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import { AppConetxt } from "../context/AppContext";
+import { FaUserLarge } from "react-icons/fa6";
 
 const Navbar = () => {
 
-  const { user, setShowLogin } = useContext(AppConetxt);
+  const { user, setShowLogin , logout } = useContext(AppConetxt);
+
+  const navigate = useNavigate();
 
 
   return (
@@ -16,9 +19,17 @@ const Navbar = () => {
         </div>
         </Link>
 
-        <div className="right-menu flex items-center justify-center gap-3 ">
+        <div className="right-menu flex items-center justify-center gap-5  ">
           {user ? 
-            <Link to='/dashboard'><button className="px-5 py-1 bg-transparent rounded-full border border-neutral-100 hover:text-neutral-300  transition-all duration-300 text-neutral-100 cursor-pointer flex justify-center items-center gap-1 text-sm sm:text-md font-medium">Go to Dashboard <IoIosArrowRoundForward /></button></Link> :   
+            (<>
+            <p  onClick={logout} className="hidden sm:flex items-center justify-center gap-1 text-neutral-400 px-3 py-1 bg-zinc-800 rounded-full cursor-pointer "><FaUserLarge className=" text-zinc-500" />Logout</p>
+
+            <Link to='/dashboard'><button className="px-5 py-1 bg-transparent rounded-full border border-neutral-100 hover:text-neutral-300  transition-all duration-300 text-neutral-100 cursor-pointer flex justify-center items-center gap-1 text-sm sm:text-md font-medium">Go to Dashboard <IoIosArrowRoundForward /></button></Link>
+            
+            
+            </>
+            )
+            :   
 
             <Link><button onClick={() => setShowLogin(true)} className="px-5 py-1 bg-blue-600 rounded-full text-neutral-100 hover:scale-105 hover:bg-blue-500 transition-all duration-300 cursor-pointer">Sign in</button></Link>
         }
